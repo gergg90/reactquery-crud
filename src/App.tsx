ada5c494218906c327bf1699b510376f9db2621b
Germain <germain.gg90@gmail.com>
2024-05-16 20:48:31 -0400

    first commit + tailwindcss + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h3>App</h3>
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
