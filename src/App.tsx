983251db01be8088e3d168d7851da74bcfb7b6b9
Germain <germain.gg90@gmail.com>
2024-05-17 08:22:44 -0400

    component theme and toogle-mode addred

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "./components/ThemeProdiver";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
-      <h3>App</h3>
-      <Button>Click</Button>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h3>App</h3>
+        <Button>Click</Button>
+      </ThemeProvider>
     </div>
   );
 }
